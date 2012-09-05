cff00885e829b8c1c51bc91c7716951258ceece4
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-09-05 12:34:43 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""
+wtforms-test
+------------
+
+Various unit test helpers for WTForms based forms.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='wtforms-test',
+    version='0.1.0',
+    url='https://github.com/kvesteri/wtforms-test',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Various unit test helpers for WTForms based forms.',
+    long_description=__doc__,
+    packages=['wtforms_test'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'WTForms>=1.0.1'
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
