96f006c8dabf6413338e929c64fdf7fe32d26e87
Will Price <willprice94+dev@gmail.com>
2014-07-25 22:35:26 +0100

    Added insert (in)valid methods

--- wtforms_test/__init__.py
+++ wtforms_test/__init__.py
@@ -1,7 +1,6 @@
 import collections
 from wtforms import Field
 import unittest
-
 from wtforms.validators import (
     DataRequired, Length, Email, Optional, NumberRange
 )
@@ -136,6 +135,19 @@ class FormTestCase(unittest.TestCase):
         field = self._get_field(field_name)
         msg = "Field '%s' is not required." % field_name
         assert self._get_validator(field, DataRequired), msg
+
+    def assert_valid(self, data):
+        self.form.process(**data)
+        self.form.validate()
+        for field_name, value in data.iteritems():
+            msg = "Field '%s' with value '%s' was valid" % (field_name, value)
+            assert field_name not in self.form.errors.keys(), msg
+
+    def assert_invalid(self, data, invalid_field):
+        self.form.process(**data)
+        self.form.validate()
+        msg = "Field '%s' with value '%s' was valid" % (invalid_field, self.form.__dict__[invalid_field].data)
+        assert invalid_field in self.form.errors.keys(), msg
 
     def assert_email(self, field_name):
         field = self._get_field(field_name)
